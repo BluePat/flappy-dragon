c39682028a4d6098c2ea14d7006c157ffbed895b
Patrik Mada <mada.patrik@gmail.com>
2022-12-10 20:26:44 +0100

    feat: Use bracket-lib to create simple terminal instance.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::all, clippy::pedantic)]
+
+use bracket_lib::prelude::*;
+
+struct State {}
+
+// Implementing a trait for a structure
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
+
+    // bracket-lib (over-)uses build pattern (quite common in Rust)
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;  // ? operator passes errors to the parrent function. Notice function signature!
+
+    // Call to start executing game loop.
+    main_loop(context, State{})
 }
