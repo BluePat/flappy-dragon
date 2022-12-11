55ddc90b4e994ab77026d684e09d8c7f67b07fc4
Patrik Mada <mada.patrik@gmail.com>
2022-12-11 01:43:43 +0100

    feat: Add player, vertical movement and gravity.

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,90 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+const TERMINAL_VELOCITY: f32 = 2.0;
+const VELOCITY_INCREMENT: f32 = 0.2;
+const VELOCITY_ON_FLAP: f32 = -2.0;
+
+struct Player {
+    x: i32,  // Progress through the level
+    y: i32,  // Vertical position in screen-space
+    velocity: f32,  // Vertical velocity
+}
+
+impl Player {
+
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        // Sets a single character on screen
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < TERMINAL_VELOCITY {
+            self.velocity += VELOCITY_INCREMENT;
+        }
+
+        self.y += self.velocity as i32;  // Rounding Down
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = VELOCITY_ON_FLAP;
+    }
+}
+
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
+
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        //TODO: Finish stub
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+
+        // Tick function will be called more often than player can react
+        // This block makes updates to the player every FRAME_DURATION ms
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        // Not restricting user input by frame time -- would made game unresponsive
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap your wings.");
+
+        // If player has fallen off the bottom of the screen, END the game
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -55,6 +125,8 @@ impl State {
     }
 
     fn restart(&mut self, ctx: &mut BTerm) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 }
