00f6c2e2ea3644d3e8c7cf394847551fd9beb717
Patrik Mada <mada.patrik@gmail.com>
2022-12-13 20:16:33 +0100

    feat: Add obstacles.

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,12 @@ const VELOCITY_INCREMENT: f32 = 0.2;
 const VELOCITY_ON_FLAP: f32 = -2.0;
 
 struct Player {
-    x: i32,  // Progress through the level
-    y: i32,  // Vertical position in screen-space
-    velocity: f32,  // Vertical velocity
+    x: i32,        // Progress through the level
+    y: i32,        // Vertical position in screen-space
+    velocity: f32, // Vertical velocity
 }
 
 impl Player {
-
     fn new(x: i32, y: i32) -> Self {
         Player {
             x,
@@ -41,7 +40,7 @@ impl Player {
             self.velocity += VELOCITY_INCREMENT;
         }
 
-        self.y += self.velocity as i32;  // Rounding Down
+        self.y += self.velocity as i32; // Rounding Down
         self.x += 1;
         if self.y < 0 {
             self.y = 0;
@@ -53,19 +52,63 @@ impl Player {
     }
 }
 
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        // Draw top half of the obstacle
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+
+        // Draw botton half of the obstacle
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_in_gap = (player.y - self.gap_y).abs() < half_size;
+
+        does_x_match && !player_in_gap
+    }
+}
+
 struct State {
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
     mode: GameMode,
+    score: i32,
 }
 
 impl State {
-
     fn new() -> Self {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
+            score: 0,
         }
     }
 
@@ -87,9 +130,17 @@ impl State {
 
         self.player.render(ctx);
         ctx.print(0, 0, "Press SPACE to flap your wings.");
+        ctx.print(0, 2, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
 
         // If player has fallen off the bottom of the screen, END the game
-        if self.player.y > SCREEN_HEIGHT {
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
@@ -103,8 +154,8 @@ impl State {
         if let Some(key) = ctx.key {
             match key {
                 VirtualKeyCode::P => self.restart(ctx),
-                VirtualKeyCode::Q => ctx.quitting = true,  // instructs bracket-lib that user is ready to terminate the app
-                _ => {},
+                VirtualKeyCode::Q => ctx.quitting = true, // instructs bracket-lib that user is ready to terminate the app
+                _ => {}
             }
         }
     }
@@ -112,14 +163,15 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You Died!");
-        ctx.print_centered(8, "(P) Play Again");
-        ctx.print_centered(10, "(Q) Quit Game");
+        ctx.print_centered(7, &format!("Score: {}", self.score));
+        ctx.print_centered(10, "(P) Play Again");
+        ctx.print_centered(12, "(Q) Quit Game");
 
         if let Some(key) = ctx.key {
             match key {
                 VirtualKeyCode::P => self.restart(ctx),
                 VirtualKeyCode::Q => ctx.quitting = true,
-                _ => {},
+                _ => {}
             }
         }
     }
@@ -127,7 +179,9 @@ impl State {
     fn restart(&mut self, ctx: &mut BTerm) {
         self.player = Player::new(5, 25);
         self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.mode = GameMode::Playing;
+        self.score = 0;
     }
 }
 
@@ -143,11 +197,10 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-
     // bracket-lib (over-)uses build pattern (quite common in Rust)
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
-        .build()?;  // ? operator passes errors to the parrent function. Notice function signature!
+        .build()?; // ? operator passes errors to the parrent function. Notice function signature!
 
     // Call to start executing game loop.
     main_loop(context, State::new())
